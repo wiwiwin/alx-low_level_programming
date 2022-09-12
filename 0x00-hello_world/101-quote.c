063e25d86b4ad3b437b429315398a6fc73d73055
wiwiwin <hermytes@gmail.com>
2022-09-12 15:04:46 -0700

    101-quote!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  *
  * Retuen: Alaway 1 (Success)
  */
-int main(void)
+int main()
 {
-	write(1, "and that piece of art is useful" " -Dora Korpar,2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" -Dora Korpar,2015-10-19\n", 59);
 	return (1);
 }
 
