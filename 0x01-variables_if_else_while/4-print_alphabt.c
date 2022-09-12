90d0d8f16dcfd8ccd7a0cb4b0bb57713936ded3c
wiwiwin <hermytes@gmail.com>
2022-09-12 16:21:36 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+/**
+ * main - alphabet in lowercase
+ *
+ * Return: Always 0(Success)
+ */
+int main(Void)
+{ 
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+	{
+		while (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
