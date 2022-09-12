54bddcc46641e75dc3bed0108532f131d09fc91c
wiwiwin <hermytes@gmail.com>
2022-09-12 16:00:19 -0700

    alphabet in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0(Seccess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
