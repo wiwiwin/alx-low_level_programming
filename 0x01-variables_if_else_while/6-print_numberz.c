7930fe0b6a138e8065302edb5080e7afd3fda004
wiwiwin <hermytes@gmail.com>
2022-09-12 17:45:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Retun: Always (Success)
+ */
+int main(void)
+{
+	int n;
+	for(n = 0; n < 10 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
