ce99789736c238397310bf86ae5d9fdc70dfe23b
wiwiwin <hermytes@gmail.com>
2022-09-14 15:12:01 -0700

    the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * prints 10 timees the alphabet in lowercase
  * Return: Always 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	char c;
@@ -13,7 +13,7 @@ void print_alphabet_x10(void);
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			print_alphabet_x10(c);
+			_putchar(c);
 		}
 		_putchar('\n');
 		return (0);
