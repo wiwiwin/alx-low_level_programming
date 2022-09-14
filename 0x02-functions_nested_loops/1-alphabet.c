d613fdda3556e171b73bd77ba5d54570ed5de397
wiwiwin <hermytes@gmail.com>
2022-09-14 05:58:54 -0700

    prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets in lowercase  
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		-putchar(j);
+	}
+	-putchar('\n');
+}
+
