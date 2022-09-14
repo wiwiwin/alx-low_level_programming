08ba5c986688d917e71b1c5735052e85a09b0ca7
wiwiwin <hermytes@gmail.com>
2022-09-14 13:41:05 -0700

    2-print_alphabet_10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@ int main(void)
 {
 	print_alphabet_x10();
 
-	char c;
-	int i;
+	char c, i;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			_printchar(c);
+			_putchar(c);
 		}
-		_printchar('\n')
-		return (0);
+		_putchar('\n')
+		
+			return (0);
 	}
 }
