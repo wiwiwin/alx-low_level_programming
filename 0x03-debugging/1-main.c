2e15280826618078e842840ff3b0e613c28a4a9f
wiwiwin <hermytes@gmail.com>
2022-09-16 13:00:07 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,11 +8,15 @@ int main(void)
 	int i;
 
 	printf("Infinte loop incoming :(\n)");
+
 	i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-	}
+
+	/**	
+	* while (i < 10)
+	* {
+	* putchar(i);
+	* }
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
