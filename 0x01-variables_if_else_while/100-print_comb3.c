e1d1547414387efa67d601a751dd834fd03691f5
wiwiwin <hermytes@gmail.com>
2022-09-18 09:59:28 -0700

    combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,16 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 1; j <= 9; i++)
+		for (j = i + 1; j <= 9; i++)
 		{
-			if (j > i)
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 9)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
