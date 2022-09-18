aaa9f458e9f628d65a25f8641662a7ba8c32abe1
wiwiwin <hermytes@gmail.com>
2022-09-18 06:31:18 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main-prints all hexadecimal numbers in lowercase 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
