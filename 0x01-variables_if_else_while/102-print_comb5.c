ed779486749576777ba090983a7074d8c7ee998e
wiwiwin <hermytes@gmail.com>
2022-09-19 03:35:56 -0700

    two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,22 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 0; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((i % 10) + '0');
-		}
-		
-		if (i != 98)
-		{
-			putchar(',');
-			putchar(' ');
+			if (j > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar((' '));
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
