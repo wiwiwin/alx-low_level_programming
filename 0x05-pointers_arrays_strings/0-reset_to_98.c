9888abc6381bc183164492a01b12c4e0cb6a30a7
wiwiwin <hermytes@gmail.com>
2022-09-19 12:54:42 -0700

    updates the value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include"main.h"
+/**
+ * reset_to_98-update the value of int to it points to to 98
+ * Return:nothing
+ */
+void rest_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes()
+ */
+
+void reset_to_98(int *n);
+
+#include<stdio.h>
+#endif /*MAIN_H*/
