5549ef6b6b4dd45de2e14b84f052b9462b8fd223
wiwiwin <hermytes@gmail.com>
2022-09-19 02:27:54 -0700

    combinations of two digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,16 @@
 int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
-				putchar((i % 10)+ '0');
-				putchar((j % 10)+ '0');
-				putchar((k % 10)+ '0');
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
 
 				if (i != 7)
 				{
