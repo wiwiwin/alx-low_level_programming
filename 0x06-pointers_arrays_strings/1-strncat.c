9300c1613ff98e3c1ec986cbf1dd81117c30f1b4
wiwiwin <hermytes@gmail.com>
2022-09-22 04:03:27 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(*dest, *src, n)
-		return (dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
+
+	return (dest);
+
 }
